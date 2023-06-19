c1f2eb17eff7c8c4a382d79f2c94d923cde11a48
gh-mbanala <53036497+saisudharshan@users.noreply.github.com>
2023-06-19 18:04:33 +0530

    Update WeatherTable.tsx

--- src/components/WeatherTable.tsx
+++ src/components/WeatherTable.tsx
@@ -11,10 +11,10 @@ const WeatherTable = (props: LayoutProps) => {
           
             <table key={i} style={{margin: "10px"}}>
             <tr>
-              <th colSpan={2} style={{background: "orange", color: "black"}}> Date: {d.dt_txt.split(" ")[0]}</th>
+              <th colSpan={2} style={{background: "orange", color: "black"}}> Date: {d.dt_txt.split(" ")[0].split("-").reverse().join("/")}</th>
             </tr>
             <tr>
-              <th colSpan={2} style={{background: "orange", color: "black"}}> Time: {d.dt_txt.split(" ")[1]}</th>
+              <th colSpan={2} style={{background: "orange", color: "black"}}> Time: {d.dt_txt.split(" ")[1].substring(0, d.dt_txt.split(" ")[1].length -3)}</th>
             </tr>
             <tr style={{background: "grey", color: "black"}}>
               <th colSpan={2}>Temperature</th>
